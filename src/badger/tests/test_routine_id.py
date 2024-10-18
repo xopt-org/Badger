fdb45b63386b0d4b5a22f4496689de261c5f4765
Shamin Chowdhury <shamin@slac.stanford.edu>
2024-10-18 10:00:59 -0700

    - Add tests for modifying routines with runs

--- src/badger/tests/test_routine_id.py
+++ src/badger/tests/test_routine_id.py
@@ -1,5 +1,4 @@
 import pytest
-import multiprocessing
 
 def test_routines_same_name():
     from badger.db import save_routine, remove_routine
@@ -18,7 +17,6 @@ def test_routines_same_name():
     remove_routine(routine2.id)
 
 def test_modify_routine_no_runs(qtbot):
-    from badger.errors import BadgerRoutineError
     from badger.gui.default.components.routine_page import BadgerRoutinePage
     from badger.db import list_routine, remove_routine, load_routine
 
@@ -39,84 +37,88 @@ def test_modify_routine_no_runs(qtbot):
     routine, _ = load_routine(id)
     window.refresh_ui(routine)
 
-    qtbot.keyClicks(window.generator_box.cb, "extremum_seeking")
-
+    index = window.generator_box.cb.findText("extremum_seeking")
+    window.generator_box.cb.setCurrentIndex(index)
     window.save()
 
     ids = list_routine()[0]
     assert len(ids) == 1
     for id in ids: remove_routine(id)
 
-'''
-class TestRoutineRunner:
-    @pytest.fixture(scope="session")
-    def init_multiprocessing(self):
-        multiprocessing.set_start_method("fork", force=True)
+# TODO: write test for modifying name of routine with runs
+def test_modify_routine_name(qtbot):
+    from badger.gui.default.components.routine_page import BadgerRoutinePage
+    from badger.db import list_routine, remove_routine, load_routine, save_run
 
-    @pytest.fixture(scope="session")
-    def init_multiprocessing_alt(self):
-        # Use 'spawn' to start new processes instead of 'fork'
-        multiprocessing.set_start_method("spawn", force=True)
+    window = BadgerRoutinePage()
+    qtbot.addWidget(window)
+    window.env_box.relative_to_curr.setChecked(False)
+    qtbot.keyClicks(window.generator_box.cb, "expected_improvement")
+    qtbot.keyClicks(window.env_box.cb, "test")
+    window.env_box.var_table.cellWidget(0, 0).setChecked(True)
+    window.env_box.obj_table.cellWidget(0, 0).setChecked(True)
 
-    @pytest.fixture
-    def process_manager(self):
-        from badger.gui.default.components.create_process import CreateProcess
-        from badger.gui.default.components.process_manager import ProcessManager
+    window.save()
+    id = list_routine()[0][0]
+    routine, _ = load_routine(id)
+    window.refresh_ui(routine)
 
-        process_manager = ProcessManager()
-        process_builder = CreateProcess()
-        process_builder.subprocess_prepared.connect(process_manager.add_to_queue)
-        process_builder.create_subprocess()
+    run = {
+        'filename': 'test_run.yaml',
+        'routine': routine,
+        'data': {
+            'f': [0.051416649999999994],
+            'timestamp': [1729263544.34316]
+            # 'x0': [-0.1711],
+            # 'xopt_error': [False],
+            # 'xopt_runtime': [0.0007418617606163025]
+        }
+    }
+    save_run(run)
 
-        yield process_manager
+    window.edit_save.setText('new_name')
+    window.save()
 
-        process_manager.close_proccesses()
+    ids = list_routine()[0]
+    assert len(ids) == 1
+    for id in ids: remove_routine(id)
 
-    @pytest.fixture
-    def instance(self, process_manager, routine):
-        from badger.db import save_routine
-        from badger.gui.default.components.routine_runner import (
-            BadgerRoutineSubprocess,
-        )
-        from badger.tests.utils import create_routine, fix_db_path_issue
+# TODO: write test for modifying algorithm of routine with runs
+def test_modify_routine_algorithm(qtbot):
+    from badger.gui.default.components.routine_page import BadgerRoutinePage
+    from badger.db import list_routine, remove_routine, load_routine, save_run
 
-        fix_db_path_issue()
+    window = BadgerRoutinePage()
+    qtbot.addWidget(window)
+    window.env_box.relative_to_curr.setChecked(False)
+    qtbot.keyClicks(window.generator_box.cb, "expected_improvement")
+    qtbot.keyClicks(window.env_box.cb, "test")
+    window.env_box.var_table.cellWidget(0, 0).setChecked(True)
+    window.env_box.obj_table.cellWidget(0, 0).setChecked(True)
 
-        instance = BadgerRoutineSubprocess(process_manager, routine)
-        instance.pause_event = multiprocessing.Event()
-        return instance
+    window.save()
+    id = list_routine()[0][0]
+    routine, _ = load_routine(id)
+    window.refresh_ui(routine)
 
-    # TODO: write test for modifying name of routine with runs
-    def test_modify_routine_name(self, qtbot, instance):
-        from badger.errors import BadgerRoutineError
-        from badger.gui.default.components.routine_page import BadgerRoutinePage
-        from badger.db import list_routine, remove_routine, load_routine
+    run = {
+        'filename': 'test_run.yaml',
+        'routine': routine,
+        'data': {
+            'f': [0.051416649999999994],
+            'timestamp': [1729263544.34316]
+            # 'x0': [-0.1711],
+            # 'xopt_error': [False],
+            # 'xopt_runtime': [0.0007418617606163025]
+        }
+    }
+    save_run(run)
 
-        window = BadgerRoutinePage()
-        qtbot.addWidget(window)
-        window.env_box.relative_to_curr.setChecked(False)
-        qtbot.keyClicks(window.generator_box.cb, "expected_improvement")
-        qtbot.keyClicks(window.env_box.cb, "test")
-        
-        window.env_box.var_table.cellWidget(0, 0).setChecked(True)
-        assert window.env_box.var_table.export_variables() == {"x0": [-1, 1]}
-        
-        window.env_box.obj_table.cellWidget(0, 0).setChecked(True)
-        assert window.env_box.obj_table.export_objectives() == {"f": "MINIMIZE"}
+    index = window.generator_box.cb.findText("extremum_seeking")
+    window.generator_box.cb.setCurrentIndex(index)
+    window.save()
 
-        window.save()
-        id = list_routine()[0][0]
-        routine, _ = load_routine(id)
-        window.refresh_ui(routine)
-        instance(routine)
-        instance.run()
-
-
-        ids = list_routine()[0]
-        for id in ids: remove_routine(id)
-
-    # TODO: write test for modifying algorithm of routine with runs
-    def test_modify_routine_algorithm(self, qtbot):
-        pass
-'''
+    ids = list_routine()[0]
+    assert len(ids) == 2
+    for id in ids: remove_routine(id)
         
