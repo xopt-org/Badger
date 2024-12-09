bd583a5ef5ea40d64f8f8c87e37b5563813e28a8
woehnert <jannik.woehnert@desy.de>
2024-12-09 18:14:41 +0100

    add XFEl filter and remove machine from tag dictionary

---
+++ src/badger/built_in_plugins/filters/XFEL/README.md
@@ -0,0 +1,5 @@
+# tags for XFEL environment
+
+## Prerequisites
+
+## Usage
---
+++ src/badger/built_in_plugins/filters/XFEL/__init__.py
@@ -0,0 +1,4 @@
+machine_tags = {'region':  ["SA1", "SA2", "SA3", "CL", "L3", "L2", "L1", "Injector"],
+                'objective': ['orbit correction', 'beam energy', 'photon intensity', 'quadrupoles'],
+                'gain': []
+                }
