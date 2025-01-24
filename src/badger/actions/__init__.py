d0165e398b002cd620a0412739cbcaecf35ed813
Zhe Zhang <zhezhang@slac.stanford.edu>
2025-01-23 22:10:19 -0800

    Only support the new GUI now

--- src/badger/actions/__init__.py
+++ src/badger/actions/__init__.py
@@ -14,7 +14,7 @@ def show_info(args):
 
     if args.gui:
         if check_n_config_paths(args.config_filepath):
-            from badger.gui.default import launch_gui
+            from badger.gui.acr import launch_gui
 
             launch_gui(config_path)
 
