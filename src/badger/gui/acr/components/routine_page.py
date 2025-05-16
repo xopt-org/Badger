ff288f231f531068d3c68f1f94f70157f14d547e
michaellans <mlans@slac.stanford.edu>
2025-05-16 12:01:42 -0700

    remove old template filepath comment

--- src/badger/gui/acr/components/routine_page.py
+++ src/badger/gui/acr/components/routine_page.py
@@ -109,9 +109,7 @@ class BadgerRoutinePage(QWidget):
         self.env_box.relative_to_curr.setChecked(True)
 
         # Template path
-        self.template_dir = "/home/physics/badger/cu_hxr/plugins/templates"
-        # self.template_dir = os.path.join(self.BADGER_PLUGIN_ROOT, "templates")
-        # self.template_dir = "/home/physics/mlans/workspace/badger_test/Badger/src/badger/built_in_plugins/templates"
+        self.template_dir = os.path.join(self.BADGER_PLUGIN_ROOT, "templates")
 
     def init_ui(self):
         config_singleton = init_settings()
