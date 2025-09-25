b44efc173ff636f6c021cb9d36505575ad70c4ec
Ryan Roussel <rroussel@slac.stanford.edu>
2025-09-25 09:40:16 -0500

    refactor history nav

--- src/badger/gui/default/components/history_navigator.py
+++ src/badger/gui/default/components/history_navigator.py
@@ -1,113 +1,62 @@
-from PyQt5.QtWidgets import QComboBox, QTreeWidget, QTreeWidgetItem
-from PyQt5.QtCore import QModelIndex, Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTreeWidget, QTreeWidgetItem
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 from badger.archive import get_base_run_filename
 from badger.utils import run_names_to_dict
 
 
-# Modified based on the following solution
-# https://stackoverflow.com/a/9672359/4263605
-class HistoryNavigator(QComboBox):
+class HistoryNavigator(QWidget):
     def __init__(self):
         super().__init__()
-        # self.view() = QTreeWidget()
-        self.setView(QTreeWidget())
-        self.setModel(self.view().model())
 
-        self.view().setHeaderHidden(True)
-        self.view().setMinimumHeight(256)
-        # self.view().setItemsExpandable(False)
-        # self.view().setRootIsDecorated(False)
-        self.runs = None  # all runs to be shown in combobox
+        # Layout for the widget
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.tree_widget = QTreeWidget()
 
-    def showPopup(self):
-        self.setRootModelIndex(QModelIndex())  # key to success!
-        QComboBox.showPopup(self)
+        self.tree_widget.setHeaderLabels(["History Navigator"])
+        header = self.tree_widget.header()
+        # Set the font of the header to bold
+        bold_font = QFont()
+        bold_font.setBold(True)
+        header.setFont(bold_font)
 
-    def _firstSelectableItem(self, parent=QModelIndex()):
+        self.tree_widget.setMinimumHeight(256)
+        layout.addWidget(self.tree_widget)
+
+        self.runs = None  # all runs to be shown in the tree widget
+        self.setStyleSheet("""
+            QTreeWidget {
+                background-color: #37414F;
+            }
+        """)
+
+    def _firstSelectableItem(self, parent=None):
         """
         Internal recursive function for finding the first selectable item.
         """
-        for i in range(self.model().rowCount(parent)):
-            itemIndex = self.model().index(i, 0, parent)
-            if self.view().itemFromIndex(itemIndex).flags() & Qt.ItemIsSelectable:
-                return parent, i
-            else:
-                itemIndex, row = self._firstSelectableItem(itemIndex)
-                if row is not None:
-                    return itemIndex, row
-        return parent, None
+        if parent is None:
+            parent = self.tree_widget.invisibleRootItem()
 
-    def currentIsFirst(self):
-        if not self.runs:
-            return True
-
-        run = get_base_run_filename(self.currentText())
-        try:
-            idx = self.runs.index(run)
-            if idx == 0:
-                return True
-            else:
-                return False
-        except:  # run is empty string
-            return True
-
-    def currentIsLast(self):
-        if not self.runs:
-            return True
-
-        run = get_base_run_filename(self.currentText())
-        try:
-            idx = self.runs.index(run)
-            tot = len(self.runs)
-            if idx == tot - 1:
-                return True
-            else:
-                return False
-        except:  # run is empty string
-            return True
-
-    def _firstMatchingSelectableItem(self, text, parent=QModelIndex()):
-        for i in range(self.model().rowCount(parent)):
-            itemIndex = self.model().index(i, 0, parent)
-            item = self.view().itemFromIndex(itemIndex)
-            if (item.flags() & Qt.ItemIsSelectable) and item.text(0).startswith(text):
-                return parent, i
-            else:
-                itemIndex, row = self._firstMatchingSelectableItem(text, itemIndex)
-                if row is not None:
-                    return itemIndex, row
-        return parent, None
-
-    def _nextSelectableItem(self, parent=QModelIndex()):
-        run_curr = get_base_run_filename(self.currentText())
-        idx = self.runs.index(run_curr)
-        run_next = self.runs[idx + 1]
-
-        return self._firstMatchingSelectableItem(run_next)
-
-    def _previousSelectableItem(self, parent=QModelIndex()):
-        run_curr = get_base_run_filename(self.currentText())
-        idx = self.runs.index(run_curr)
-        run_prev = self.runs[idx - 1]
-
-        return self._firstMatchingSelectableItem(run_prev)
-
-    def _currentSelectableItem(self, parent=QModelIndex()):
-        run_curr = get_base_run_filename(self.currentText())
-
-        return self._firstMatchingSelectableItem(run_curr)
+        for i in range(parent.childCount()):
+            item = parent.child(i)
+            if item.flags() & Qt.ItemIsSelectable:
+                return item
+            result = self._firstSelectableItem(item)
+            if result:
+                return result
+        return None
 
     def updateItems(self, runs=None):
-        self.view().clear()
-
-        self.runs = runs  # store the runs for nav
+        self.tree_widget.clear()
+        self.runs = runs  # store the runs for navigation
         if runs is None:
             return
 
-        items = []
-        flag_first_item = True
-        first_items = []
         runs_dict = run_names_to_dict(runs)
+        first_items = []
+        flag_first_item = True
+
         for year, dict_year in runs_dict.items():
             item_year = QTreeWidgetItem([year])
             item_year.setFlags(item_year.flags() & ~Qt.ItemIsSelectable)
@@ -130,38 +79,53 @@ class HistoryNavigator(QComboBox):
                         first_items.append(item_day)
                         flag_first_item = False
 
-                    for i, file in enumerate(list_day):
+                    for file in list_day:
                         item_file = QTreeWidgetItem([file])
                         item_day.addChild(item_file)
                     item_month.addChild(item_day)
                 item_year.addChild(item_month)
-            items.append(item_year)
-        self.view().insertTopLevelItems(0, items)
+            self.tree_widget.addTopLevelItem(item_year)
+
+        # Expand the first set of items
         for item in first_items:
             item.setExpanded(True)
 
-        parent, row = self._firstSelectableItem()  # key to success!
-        if row is not None:
-            self.setRootModelIndex(parent)
-            self.setCurrentIndex(row)
-
     def selectNextItem(self):
-        parent, row = self._nextSelectableItem()
-        if row is not None:
-            self.setRootModelIndex(parent)
-            self.setCurrentIndex(row)
+        run_curr = get_base_run_filename(self.currentText())
+        idx = self.runs.index(run_curr)
+        if idx < len(self.runs) - 1:
+            self._selectItemByRun(self.runs[idx + 1])
 
     def selectPreviousItem(self):
-        parent, row = self._previousSelectableItem()
-        if row is not None:
-            self.setRootModelIndex(parent)
-            self.setCurrentIndex(row)
+        run_curr = get_base_run_filename(self.currentText())
+        idx = self.runs.index(run_curr)
+        if idx > 0:
+            self._selectItemByRun(self.runs[idx - 1])
 
-    def changeCurrentItem(self, text, color=None):
-        parent, row = self._currentSelectableItem()
-        if row is not None:
-            itemIndex = self.model().index(row, 0, parent)
-            item = self.view().itemFromIndex(itemIndex)
-            item.setText(0, text)
-            if color:
-                item.setForeground(0, color)
+    def _selectItemByRun(self, run):
+        """
+        Internal function to select a tree widget item by run name.
+        """
+        for i in range(self.tree_widget.topLevelItemCount()):
+            year_item = self.tree_widget.topLevelItem(i)
+            for j in range(year_item.childCount()):
+                month_item = year_item.child(j)
+                for k in range(month_item.childCount()):
+                    day_item = month_item.child(k)
+                    for _l in range(day_item.childCount()):
+                        file_item = day_item.child(_l)
+                        if get_base_run_filename(file_item.text(0)) == run:
+                            self.tree_widget.setCurrentItem(file_item)
+                            return
+
+    def currentText(self):
+        current_item = self.tree_widget.currentItem()
+        if current_item:
+            return current_item.text(0)
+        return ""
+
+    def count(self):
+        if self.runs is None:
+            return 0
+
+        return len(self.runs)
