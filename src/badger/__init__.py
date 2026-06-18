f8b562530a2b0a54103017d72006f3bfe5350c67
Sara Miskovich <saramiskovich@gmail.com>
2026-06-18 16:31:51 -0700

    Simplified code

--- src/badger/__init__.py
+++ src/badger/__init__.py
@@ -1,11 +1,9 @@
-import os as _os
+import os
 
 _DEFAULT_NUM_THREADS = "4"
 
 for _var in ("OMP_NUM_THREADS", "MKL_NUM_THREADS", "OPENBLAS_NUM_THREADS"):
-    _os.environ.setdefault(_var, _DEFAULT_NUM_THREADS)
-
-del _os, _var, _DEFAULT_NUM_THREADS
+    os.environ.setdefault(_var, _DEFAULT_NUM_THREADS)
 
 try:
     from badger._version import __version__
