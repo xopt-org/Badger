a32654668b77e39fa6f4eff63a8b7082a7ef915d
michaellans <mlans@slac.stanford.edu>
2026-09-14 18:05:19 -0700

    add BadgerTerminationReachedDialog to supress_popups mocker for testing

--- src/badger/tests/conftest.py
+++ src/badger/tests/conftest.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import pytest
+from PyQt5.QtWidgets import QDialog
 
 
 @pytest.fixture(autouse=True)
@@ -8,6 +9,10 @@ def suppress_popups(mocker):
     mocker.patch(
         "badger.gui.windows.expandable_message_box.ExpandableMessageBox.exec_",
         return_value=None,
+    )
+    mocker.patch(
+        "badger.gui.windows.termination_reached_dialog.BadgerTerminationReachedDialog.exec_",
+        return_value=QDialog.Rejected,
     )
 
 
